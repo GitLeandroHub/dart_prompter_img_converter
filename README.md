3fe224012660d4d723ef4a43ba5436f3c5c1509d
Leandro Jareño Damião <leandro.jareno@gmail.com>
2020-12-15 18:13:09 -0300

    Add all my converter files

---
+++ README.md
@@ -0,0 +1,17 @@
+A simple prompter like program made using the dart language. The user uses command-line navigation to choose a file and convert the end extention.
+
+To execute the pogram:
+$ dart main.dart
+
+This program uses a package previous created by me as part of the couse too, folow:
+- https://pub.dev/packages/prompter_ljd
+
+The image used to change the extension can be found here:
+https://www.positive.news/environment/people-share-their-love-for-trees-and-forests-on-international-day-of-forests/
+Saved into jpg and converted to png as shows in the gif.
+
+This Flutter project was build as a part of the Dart and Flutter: The Complete Developer's Guide course.
+
+https://www.udemy.com/certificate/UC-e1602f4f-5fbe-4d08-a36c-68a2291d935d/
+
+![](converter.gif)
---
+++ bin/main.dart
@@ -0,0 +1,45 @@
+import 'package:prompter_ljd/prompter_ljd.dart';
+import 'dart:io';
+import 'package:converter/src/converter.dart';
+
+void main() {
+  final prompter = new Prompter();
+
+  final choice = prompter.askBinary('Are you here to convert an image?');
+  if (!choice) {
+    exit(0);
+  }
+
+  final format = prompter.askMultiple('Select format:', buildFormatOptions());
+  final selectedFile =
+      prompter.askMultiple('Select an image to convert:', buildFileOptions());
+
+  final newPath = convertImage(selectedFile, format);
+
+  final shouldOpen = prompter.askBinary('Open the image?');
+  print(selectedFile.path);
+
+  if (shouldOpen) {
+    Process.start('.', [newPath]);
+  }
+}
+
+List<Option> buildFormatOptions() {
+  return [
+    new Option('Convert to jpeg', 'jpeg'),
+    new Option('Convert to png', 'png')
+  ];
+}
+
+List<Option> buildFileOptions() {
+  //Get a reference to the current working directory
+  return Directory.current.listSync().where((entity) {
+    return FileSystemEntity.isFileSync(entity.path) &&
+        entity.path.contains(new RegExp(r'\.(png|jpg|jpeg)'));
+  }).map((entity) {
+    final filename = entity.path.split(Platform.pathSeparator).last;
+    return new Option(filename, entity);
+  }).toList();
+
+  //Take all the images and create an option object out of each
+}
